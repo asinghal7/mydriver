626e5d5326706f13c97a43d56f5d7fc6e799385f
Akshat Singhal <akshatsinghal7@gmail.com>
2020-07-28 17:41:20 +0530

    added checks in sysfs and removed minor bugs

--- mydriver.c
+++ mydriver.c
@@ -43,24 +43,12 @@ struct my_dev {
         struct my_aes_ctx my_ctx;
 };
 
-// struct my_aes_ctx {
-//         struct crypto_cipher *fallback;
-//         struct aes_key enc_key;
-//         struct aes_key dec_key;
-// };
-
 static struct my_dev *my_devs;
 
 static int __init my_driver_init(void);
 static void __exit my_driver_exit(void);
 
 static int my_aes_init(struct my_dev *dev);
-// static int my_aes_init(struct crypto_tfm *tfm);
-// static void my_aes_exit(struct crypto_tfm *tfm);
-// static int my_aes_setkey(struct crypto_tfm *tfm, const u8 *key,
-//                                 unsigned int keylen);
-// static void my_aes_encrypt(struct crypto_tfm *tfm, u8 *dst, const u8 *src);
-// static void my_aes_decrypt(struct crypto_tfm *tfm, u8 *dst, const u8 *src);
 
 /*************** Driver Fuctions **********************/
 static int my_open(struct inode *inode, struct file *file);
@@ -131,120 +119,16 @@ static void my_aes_exit(struct my_dev *dev)
         kfree(dev->my_ctx.key);
 }
 
-// static int my_aes_init(struct crypto_tfm *tfm)
-// {
-//         const char *alg = crypto_tfm_alg_name(tfm);
-//         struct crypto_cipher *fallback;
-//         struct my_aes_ctx = crypto_tfm_ctx(tfm);
-
-//         fallback = crypto_alloc_cipher(alg, 0, CRYPTO_ALG_NEED_FALLBACK);
-//         if (IS_ERR(fallback)) {
-//                 printk(KERN_ERR 
-//                         "Failed to allocate tranformation for '%s': %ld\n",
-//                         alg, PTR_ERR(fallback));
-//                 return PTR_ERR(fallback);
-//         }
-        
-//         crypto_cipher_set_flags(fallback, 
-//                                 crypto_cipher_get_flags((struct crypto_cipher *)
-//                                                                 tfm));
-        
-//         ctx->fallback = fallback;
-        
-//         return 0;
-// }
-
-// static void my_aes_exit(struct crypto_tfm *tfm)
-// {
-//         struct my_aes_ctx *ctx = crypto_tfm_ctx(tfm);
-
-//         if (ctx->fallback) {
-//                 crypto_free_cipher(ctx->fallback);
-//                 ctx->fallback = NULL;
-//         }
-// }
-
-// static int my_aes_setkey(struct crypto_tfm *tfm, const u8 *key,
-//                                 unsigned int keylen)
-// {
-//         int ret;
-//         struct my_aes_ctx *ctx = crypto_tfm_ctx(tfm);
-
-//         preempt_disable();
-//         pagefault_disable();
-//         enable_kernel_vsx();
-//         ret = aes_my_set_encrypt_key(key, keylen * 8, &ctx->enc_key);
-//         ret |= aes_my_set_decrypt_key(key, keylen * 8, &ctx->dec_key);
-//         disable_kernel_vsx();
-//         pagefault_enable();
-//         preempt_enable();
-
-//         ret |= crypto_cipher_setkey(ctx->fallback, key, keylen);
-
-//         return ret ? -EINVAL : 0;
-// }
-
-// static void my_aes_encrypt(struct crypto_tfm *tfm, u8 *dst, const u8 *src)
-// {
-//         struct my_aes_ctx *ctx = crypto_tfm_ctx(tfm);
-
-//         if (!crypto_simd_usable()) {
-//                 crypto_cipher_encrypt_one(ctx->fallback, dst, src);
-//         } else {
-//                 preempt_disable();
-//                 pagefault_disable();
-//                 enable_kernel_vsx();
-//                 aes_my_encrypt(src, dst, &ctx->enc_key);
-//                 disable_kernel_vsx();
-//                 pagefault_enable();
-//                 preempt_enable();
-//         }
-// }
-
-// static void my_aes_decrypt(struct crypto_tfm *tfm, u8 *dst, const u8 *src)
-// {
-//         struct my_aes_ctx *ctx = crypto_tfm_ctx(tfm);
-
-//         if(!crypto_simd_usable()) {
-//                 crypto_cipher_decrypt_one(ctx->fallback, dst, src);
-//         } else {
-//                 preempt_disable();
-//                 pagefault_disable();
-//                 enable_kernel_vsx();
-//                 aes_my_decrypt(src, dst, &ctx->dec_key);
-//                 disable_kernel_vsx();
-//                 pagefault_enable();
-//                 preempt_enable();
-//         }
-// }
-
-// struct crypto_alg my_aes_alg = {
-//         .cra_name = "aes",
-//         .cra_driver_name = "mydriver",
-//         .cra_module = THIS_MODULE,
-//         .cra_priority = 1000,
-//         .cra_type = NULL,
-//         .cra_flags = CRYPTO_ALG_TYPE_CIPHER | CRYPTO_ALG_NEED_FALLBACK,
-//         .cra_alignmask = 0,
-//         .cra_blocksize = AES_BLOCK_SIZE,
-//         .cra_ctxsize = sizeof(struct p8_aes_ctx),
-//         .cra_init = p8_aes_init,
-//         .cra_exit = p8_aes_exit,
-//         .cra_cipher = {
-//                        .cia_min_keysize = AES_MIN_KEY_SIZE,
-//                        .cia_max_keysize = AES_MAX_KEY_SIZE,
-//                        .cia_setkey = p8_aes_setkey,
-//                        .cia_encrypt = p8_aes_encrypt,
-//                        .cia_decrypt = p8_aes_decrypt,
-//         },
-// };
-
 static ssize_t sysfs_show(struct kobject *kobj, 
                 struct kobj_attribute *attr, char *buf)
 {       
         //printk(KERN_INFO "Sysfs - Read!\n");
         if (strcmp(attr->attr.name, "my_mode") == 0){
-                return sprintf(buf, "%d\n", my_mode);
+                if (my_bmode == 1) {
+                        return sprintf(buf, "Normal-0 Uppercase-1 ROT13-2 AES_encrypt-3 AES_decrypt-4 -> [%d]\n", my_mode);
+                }
+                else
+                        return sprintf(buf, "Normal-0 Uppercase-1 ROT13-2 -> [%d]\n", my_mode);
         } else if (strcmp(attr->attr.name, "my_bmode") == 0){
                 return sprintf(buf, "%d\n", my_bmode);
         } else if (strcmp(attr->attr.name, "my_bsize") == 0 && my_bmode == 1){
@@ -268,24 +152,27 @@ static ssize_t sysfs_store(struct kobject *kobj,
          * Mode 1: Converting user input to uppercase
          * Mode 2: ROT13 encryption/decryption
          */
-
         mutex_lock(&sys_mutex);
         if (strcmp(attr->attr.name, "my_mode") == 0) {
-                // if (my_mode >= 3){
-                //         my_bsize = crypto_cipher_blocksize(my_ctx->tfm);
-                //         my_bmode = 1;
-                // }
                 sscanf(buf, "%d\n", &my_mode);
-        } else if (strcmp(attr->attr.name, "my_bmode") == 0) {
-                // if (my_mode >= 3)
-                //         ;
-                // else
+                if (my_bmode == 1 && my_mode > 2 && my_mode < 5) {
+                        my_bsize = AES_BLOCK_SIZE;
+                } else if (my_mode < 3 && my_mode >= 0) {
+                } else {
+                        printk(KERN_ERR "Error: Mode out of bounds\nMode Reset");
+                        my_mode = 0;
+                }
+        } else if (strcmp(attr->attr.name, "my_bmode") == 0) {                
                 sscanf(buf, "%d\n", &my_bmode);
+                if (my_bmode != 0 && my_bmode != 1) {
+                        printk(KERN_ERR "Error: Block Mode out of bounds\nBlock Mode Reset\n");
+                        my_bmode = 0;
+                }
+                if (my_bmode == 0 && my_mode > 2)
+                        my_mode = 0;
         } else if (strcmp(attr->attr.name, "my_bsize") == 0 && my_bmode == 1) {
-                // if (my_mode >= 3)
-                //         ;
-                // else
-                sscanf(buf, "%ld\n", &my_bsize);
+                if (my_mode < 3)
+                        sscanf(buf, "%ld\n", &my_bsize);
         }
         mutex_unlock(&sys_mutex);
         return count;
@@ -352,6 +239,11 @@ static ssize_t my_read(struct file *filp,
         }
         /* read data from my_data->buffer to user buffer */
         if (my_rdbmode == 1){
+                if (my_rdbsize == 0) {
+                        printk(KERN_ALERT "Error: Block Size not set");
+                        mutex_unlock(&dev->mutex);
+                        return -1;
+                }
                 if (len % my_rdbsize) {
                         numblocks = (len / my_rdbsize) + 1;
                 } else {    
@@ -360,36 +252,31 @@ static ssize_t my_read(struct file *filp,
                 for (i = 0; i < numblocks ; i++){
                         memset(tmp, 0, my_rdbsize * sizeof(char *));
                         for (j = 0; j < my_rdbsize; j++) {
-                                if (i == (numblocks - 1) && j == ((len % my_rdbsize) - 1) && my_rdmode < 3 )
-                                        break;
                                 *(tmp + j) = *(dev->data + *offset + (i * my_rdbsize) + j);
-                                printk(KERN_INFO "data in device %x\n", *(dev->data + *offset + (i * my_rdbsize) + j));
                         }
                         if (my_rdmode == 4) {
                                 crypto_cipher_decrypt_one(dev->my_ctx.tfm, dec, tmp);
                                 for (j = 0; j < my_rdbsize; j++) {
-                                        printk(KERN_INFO "data from device %x\n", *(tmp + j));
                                         *(tmp + j) = *(dec + j);
-                                        printk(KERN_INFO "decoded data %x\n", *(dec + j));
                                 }
                         }
-                        if (i == (numblocks - 1) && my_rdmode < 3) {
-                                if (copy_to_user(user_buffer + (i * my_rdbsize), tmp, (len % my_rdbsize) - 1))
-                                        return -EFAULT;                        
-                        } else {
-                                if (copy_to_user(user_buffer + (i * my_rdbsize), tmp, my_rdbsize))
-                                        return -EFAULT;
-                        }
+                        if (copy_to_user(user_buffer + (i * my_rdbsize), tmp, my_rdbsize))
+                                        goto fault;
                 }
         } else {
                 if (copy_to_user(user_buffer, dev->data + *offset, len))
-                        return -EFAULT;
+                        goto fault;
         }
         if (my_rdbmode == 1)
                 kfree(tmp);
         *offset += len;
         mutex_unlock(&dev->mutex);
         return len;
+        fault:
+        if (my_rdbmode == 1)
+                kfree(tmp);
+        mutex_unlock(&dev->mutex);
+        return -EFAULT;
 }
 static ssize_t my_write(struct file *filp,
                 const char __user *user_buffer, size_t size, loff_t *offset)
@@ -454,29 +341,22 @@ static ssize_t my_write(struct file *filp,
                                         *(tmp + j) += ('A' - 'a');
                         } else if (my_wrmode == 2) {
                                 for (j = 0; j < my_wrbsize; j++) {
-                                        if (strcmp(tmp + j, "m") > 0)
+                                        if (strcmp(tmp + j, "n") > 0)
                                                 *(tmp + j) -= ('n' - 'a');
                                         else
                                                 *(tmp + j) += ('n' - 'a');
                                 }
                         } else if (my_wrmode == 3) {
-                                
                                 crypto_cipher_encrypt_one(dev->my_ctx.tfm, enc, tmp);
                                 for (j = 0; j < my_wrbsize; j++) {
-                                        printk(KERN_INFO "data from user buffer %x\n", *(tmp + j));
                                         *(tmp + j) = *(enc + j);
-                                        printk(KERN_INFO "encoded data %x\n", *(enc + j));
                                 }
-                        } /*else if (my_wrmode == 4) {
-                                crypto_cipher_decrypt_one(dev->my_ctx.tfm, enc, tmp);
-                                for (j = 0; j < my_wrbsize; j++)
-                                        *(tmp + j) = *(enc + j);
-                        }*/
+                        }
                         for (j = 0; j < my_wrbsize; j++){
-                                if (i == (numblocks - 1) && j == ((size % my_wrbsize) - 1) && my_wrmode != 3)
-                                        break;
-                                *(dev->data + (i * my_wrbsize) + j)= *(tmp + j);        /*TODO use memcpy instead of byte copying*/
-                                printk(KERN_INFO "data written into device %x\n", *(dev->data + (i * my_wrbsize) + j));
+                                if (i == (numblocks - 1) && j == ((size % my_wrbsize) - 1) && my_wrmode < 3)
+                                        *(dev->data + (i * my_wrbsize) + j) = '\n';
+                                else
+                                        *(dev->data + (i * my_wrbsize) + j)= *(tmp + j);        /*TODO use memcpy instead of byte copying*/
                         }
                 }
         }
@@ -491,14 +371,17 @@ static ssize_t my_write(struct file *filp,
                                 *(tmp + i) += ('A' - 'a');
                 } else if (my_wrmode == 2) {
                         for (i = 0; i < (size - 1); i++) {
-                                if (strcmp(tmp + i, "m") > 0)
+                                if (strcmp(tmp + i, "n") > 0)
                                         *(tmp + i) -= ('n' - 'a');
                                 else
                                         *(tmp + i) += ('n' - 'a');
                         }
                 }
-                for (i = 0; i < (size - 1); i++){
-                        *(dev->data + i) = *(tmp + i);
+                for (i = 0; i < size; i++){
+                        if (i == size - 1)
+                                *(dev->data + i) = '\n';
+                        else        
+                                *(dev->data + i) = *(tmp + i);
                 }
         }
         if (my_wrmode >= 3) {
