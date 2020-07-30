3bec9916ecf7608a5ba955d18fef9bb7305b1999
Akshat Singhal <akshatsinghal7@gmail.com>
2020-07-30 14:02:23 +0530

    mydriver.c with sysfs without blockmode

--- mydriver.c
+++ mydriver.c
@@ -1,19 +1,26 @@
+#include <linux/kernel.h>
 #include <linux/init.h>
 #include <linux/module.h>
-#include <linux/kernel.h>   /* printk() */
-#include <linux/slab.h>     /* kmalloc() */
-#include <linux/fs.h>       /* everything... */
-#include <linux/errno.h>    /* error codes */
-#include <linux/types.h>    /* size_t */
-#include <linux/fcntl.h>    /* O_ACCMODE */
+#include <linux/kdev_t.h>
+#include <linux/fs.h>
 #include <linux/cdev.h>
-#include <asm/uaccess.h>    /* copy_*_user */
+#include <linux/device.h>
+#include<linux/slab.h>                 //kmalloc()
+#include<linux/uaccess.h>              //copy_to/from_user()
+#include<linux/sysfs.h>
+#include<linux/kobject.h>
 MODULE_LICENSE("Dual BSD/GPL");
 MODULE_AUTHOR("Akshat");
 
-int mydriver_major = 0;
-int mydriver_minor = 0;
-int my_nr_devs = 2;
+static int my_mode = 0;
+static int mydriver_major = 0;
+static int mydriver_minor = 0;
+static int my_nr_devs = 2;
+
+static DEFINE_MUTEX(sys_mutex);
+
+static struct class *dev_class;
+static struct kobject *kobj_ref;
 
 struct my_dev {
         void *data;
@@ -21,7 +28,77 @@ struct my_dev {
         struct mutex mutex;
         struct cdev cdev;
 };
-struct my_dev *my_devs;
+static struct my_dev *my_devs;
+
+static int __init my_driver_init(void);
+static void __exit my_driver_exit(void);
+
+/*************** Driver Fuctions **********************/
+static int my_open(struct inode *inode, struct file *file);
+static int my_release(struct inode *inode, struct file *file);
+static ssize_t my_read(struct file *filp, char __user *user_buffer,
+                                        size_t size, loff_t *offset);
+static ssize_t my_write(struct file *filp, const char *user_buffer,
+                                        size_t size, loff_t *offset);
+ 
+/*************** Sysfs Fuctions **********************/
+static ssize_t sysfs_show(struct kobject *kobj,
+                struct kobj_attribute *attr, char *buf);
+static ssize_t sysfs_store(struct kobject *kobj, 
+                struct kobj_attribute *attr,const char *buf, size_t count);
+static struct kobj_attribute my_attr_mode =  __ATTR(my_mode, 0660,
+                                                        sysfs_show, sysfs_store);
+
+static struct attribute *attrs[] = {
+        &my_attr_mode.attr,
+        NULL,
+};
+
+static struct attribute_group attr_group = {
+    .attrs = attrs,
+};
+
+static struct file_operations my_fops =
+{
+        .owner          = THIS_MODULE,
+        .read           = my_read,
+        .write          = my_write,
+        .open           = my_open,
+        .release        = my_release,
+};
+
+static ssize_t sysfs_show(struct kobject *kobj, 
+                struct kobj_attribute *attr, char *buf)
+{       
+        //printk(KERN_INFO "Sysfs - Read!\n");
+        if (strcmp(attr->attr.name, "my_mode") == 0){
+                return sprintf(buf, "%d\n", my_mode);
+        }
+        return 0;
+}
+
+/*
+ * In case the program is at evaluation the of condition for the 
+ * else if case for mode '1' and another process changes my_mode 
+ * from '2' to '0', the program will fall through to the dummy value. 
+ * This is undesirable and hence calls for the use of a lock.
+ */
+static ssize_t sysfs_store(struct kobject *kobj,
+                struct kobj_attribute *attr, const char *buf, size_t count)
+{
+        //printk(KERN_INFO "Sysfs - Write!\n");
+        /*
+         * Mode 0: Text in user input format 
+         * Mode 1: Converting user input to uppercase
+         * Mode 2: ROT13 encryption/decryption
+         */
+        mutex_lock(&sys_mutex);
+        if (strcmp(attr->attr.name, "my_mode") == 0) {
+            sscanf(buf, "%d\n", &my_mode);
+        }
+        mutex_unlock(&sys_mutex);
+        return count;
+}
 
 int my_trim(struct my_dev *dev)
 {
@@ -32,32 +109,13 @@ int my_trim(struct my_dev *dev)
         return 0;
 }
 
-void my_cleanup_module(void)
-{
-        int i;
-        dev_t devno = MKDEV(mydriver_major, mydriver_minor);    /* Get rid of our char dev entries. */
-        if (my_devs) {
-                for (i = 0; i < my_nr_devs; i++) {
-                        my_trim(my_devs + i);
-                        cdev_del(&my_devs[i].cdev);
-                }
-                kfree(my_devs);
-        }    /* cleanup_module is never called if registering failed. */
-        unregister_chrdev_region(devno, my_nr_devs);
-        printk(KERN_INFO "mydriver: cleanup success\n");
-}
 
 static int my_open(struct inode *inode, struct file *file)
-{
+{       
+
         struct my_dev *dev = container_of(inode->i_cdev, struct my_dev, cdev);
         /* validate access to device */
         file->private_data = dev;
-        /*
-         * There might be an issue in case write is requested on the same device
-         * by separate processess. It might truncate the file (my_trim) while the 
-         * second process is not finished writing.
-         * Thus, a locking mechanism might be required for the same. 
-         */
         if ((file->f_flags & O_ACCMODE) == O_WRONLY) {
                 if (mutex_lock_interruptible(&dev->mutex))
                         return -ERESTARTSYS;    
@@ -65,26 +123,28 @@ static int my_open(struct inode *inode, struct file *file)
                 mutex_unlock(&dev->mutex);
         
         }
-	printk(KERN_DEBUG "process %i (%s) success open minor(%u) file\n", current->pid, current->comm, iminor(inode));
+        printk(KERN_DEBUG "process %i (%s) success open minor(%u) file\n", current->pid, current->comm, iminor(inode));
         return 0;
 }
 
-int my_release(struct inode *inode, struct file *file)
+static int my_release(struct inode *inode, struct file *file)
 {
         printk(KERN_DEBUG "process %i (%s) success release minor(%u) file\n", current->pid, current->comm, iminor(inode));
         return 0;
 }
 
-static ssize_t my_read(struct file *file, char __user *user_buffer,
-                        size_t size, loff_t *offset)
+static ssize_t my_read(struct file *filp, 
+                char __user *user_buffer, size_t size, loff_t *offset)
 {
-        struct my_dev *dev = (struct my_dev *) file->private_data;
+        struct my_dev *dev = (struct my_dev *) filp->private_data;
         ssize_t len = min_t(ssize_t, dev->size - *offset, size);
+        
         if (mutex_lock_interruptible(&dev->mutex))
                 return -ERESTARTSYS;
-        if (len <= 0)
+        if (len <= 0){
+                mutex_unlock(&dev->mutex);
                 return 0;
-
+        }
         /* read data from my_data->buffer to user buffer */
         if (copy_to_user(user_buffer, dev->data + *offset, len))
                 return -EFAULT;
@@ -93,15 +153,15 @@ static ssize_t my_read(struct file *file, char __user *user_buffer,
         mutex_unlock(&dev->mutex);
         return len;
 }
-
-
-static ssize_t my_write(struct file *file, const char __user *user_buffer,
-                                size_t size, loff_t * offset)
-{	
+static ssize_t my_write(struct file *filp,
+                const char __user *user_buffer, size_t size, loff_t *offset)
+{
         ssize_t fullsize = 1000;
-        struct my_dev *dev = (struct my_dev *) file->private_data;
+        
+        struct my_dev *dev = (struct my_dev *) filp->private_data;
         ssize_t len = min_t(ssize_t, fullsize - (dev->size - *offset), size);
         ssize_t retval = -ENOMEM;
+        
         if (mutex_lock_interruptible(&dev->mutex))
                 return -ERESTARTSYS;
         if (len <= 0)
@@ -126,42 +186,10 @@ static ssize_t my_write(struct file *file, const char __user *user_buffer,
         return retval;
 }
 
-loff_t my_llseek(struct file *filp, loff_t off, int whence)
-{
-        struct my_dev *dev = filp->private_data;
-        loff_t newpos;    
-        switch(whence) {
-        case 0: /* SEEK_SET */
-                newpos = off;
-                break;      
-        case 1: /* SEEK_CUR */
-                newpos = filp->f_pos + off;
-                break;      
-        case 2: /* SEEK_END */
-                newpos = dev->size + off;
-                break;      
-        default: /* can't happen */
-                return -EINVAL;
-        }
-        if (newpos < 0)
-                return -EINVAL;
-        filp->f_pos = newpos;
-        return newpos;
-}
-
-const struct file_operations my_fops = {
-        .owner = THIS_MODULE,
-        .open = my_open,
-        .read = my_read,
-        .write = my_write,
-        .release = my_release,
-        .llseek =   my_llseek,
-};
-
 /*
  * Set up the char_dev structure for this device.
  */
-static void my_setup_cdev(struct my_dev *dev, int index)
+static int my_setup_cdev(struct my_dev *dev, int index)
 {
         int err, devno = MKDEV(mydriver_major, mydriver_minor + index);    
         cdev_init(&dev->cdev, &my_fops);
@@ -172,50 +200,96 @@ static void my_setup_cdev(struct my_dev *dev, int index)
         if (err)
                 printk(KERN_NOTICE "Error %d adding mydriver%d", err, index);
         else
-		printk(KERN_INFO "mydriver: %d add success\n", index);
+                printk(KERN_INFO "mydriver: %d add success\n", index);
+        return err;
 }
 
-int my_init_module(void)
+static int __init my_driver_init(void)
 {
         int result, i;
-        dev_t dev = 0;    
-        /*
-         * Get a range of minor numbers to work with, asking for a dynamic major
-         * unless directed otherwise at load time.
-         */
-        if (mydriver_major) {
-                dev = MKDEV(mydriver_major, mydriver_minor);
-                result = register_chrdev_region(dev, my_nr_devs, "mydriver");
-        } else {
-                result = alloc_chrdev_region(&dev, mydriver_minor, my_nr_devs, "mydriver");
-                mydriver_major = MAJOR(dev);
+        dev_t dev = 0;
+
+        /*Allocating Major number*/
+        if ((alloc_chrdev_region(&dev, mydriver_minor, my_nr_devs, "mydriver")) <0) {
+                printk(KERN_INFO "Cannot allocate major number\n");
+                return -1;
         }
-        if (result < 0) {
-                printk(KERN_WARNING "mydriver: can't get major %d\n", mydriver_major);
-                return result;
-        } else {
-                printk(KERN_INFO "mydriver: get major %d success\n", mydriver_major);
-        }
-        /*
-         * Allocate the devices. This must be dynamic as the device number can
-         * be specified at load time.
-         */
+        mydriver_minor = MINOR(dev);
+        mydriver_major = MAJOR(dev);
+        printk(KERN_INFO "Major = %d Minor = %d \n", mydriver_major, mydriver_minor);
+        
         my_devs = kmalloc(my_nr_devs * sizeof(struct my_dev), GFP_KERNEL);
         if (!my_devs) {
                 result = -ENOMEM;
-                goto fail;
+                goto r_class;
         }
         memset(my_devs, 0, my_nr_devs * sizeof(struct my_dev));        /* Initialize each device. */
         for (i = 0; i < my_nr_devs; i++) {
                 mutex_init(&my_devs[i].mutex);
-                my_setup_cdev(&my_devs[i], i);
-        }    
-        return 0; /* succeed */  
-        fail:
-        my_cleanup_module();
-        return result;
+                if (my_setup_cdev(&my_devs[i], i)<0)
+                        goto r_class;
+        }
+
+        /*Creating struct class*/
+        if ((dev_class = class_create(THIS_MODULE, "my_class")) == NULL) {
+            printk(KERN_INFO "Cannot create the struct class\n");
+            goto r_class;
+        }
+
+        /*Creating device*/
+        if ((device_create(dev_class, NULL, dev, NULL, "my_device")) == NULL) {
+            printk(KERN_INFO "Cannot create the Device 1\n");
+            goto r_device;
+        }
+
+        /*Creating a directory in /sys/kernel/ */
+        kobj_ref = kobject_create_and_add("my_sysfs", kernel_kobj);
+        /*Creating sysfs files*/     
+        if (sysfs_create_group(kobj_ref, &attr_group)) {
+                printk(KERN_INFO"Cannot create sysfs files...\n");
+                goto r_sysfs;
+        }
+        printk(KERN_INFO "my_driver: intialization success\n");
+
+        return 0;
+
+r_sysfs:
+        kobject_put(kobj_ref); 
+        sysfs_remove_group(kernel_kobj, &attr_group);
+
+r_device:
+        class_destroy(dev_class);
+r_class:
+        if (my_devs) {
+                for (i = 0; i < my_nr_devs; i++) {
+                        my_trim(my_devs + i);
+                        cdev_del(&my_devs[i].cdev);
+                }
+                kfree(my_devs);
+        }    /* cleanup_module is never called if registering failed. */
+        unregister_chrdev_region(dev, my_nr_devs);
+        return -1;
 }
 
+void __exit my_driver_exit(void)
+{       
+        int i;
+        dev_t devno = MKDEV(mydriver_major, mydriver_minor);    /* Get rid of our char dev entries. */
 
-module_init(my_init_module);
-module_exit(my_cleanup_module);
+        kobject_put(kobj_ref);
+        sysfs_remove_group(kernel_kobj, &attr_group);
+        device_destroy(dev_class, devno);
+        class_destroy(dev_class);
+        if (my_devs) {
+                for (i = 0; i < my_nr_devs; i++) {
+                        my_trim(my_devs + i);
+                        cdev_del(&my_devs[i].cdev);
+                }
+                kfree(my_devs);
+        }    /* cleanup_module is never called if registering failed. */
+        unregister_chrdev_region(devno, my_nr_devs);
+        printk(KERN_INFO "my_driver: cleanup success\n");
+}
+
+module_init(my_driver_init);
+module_exit(my_driver_exit);
